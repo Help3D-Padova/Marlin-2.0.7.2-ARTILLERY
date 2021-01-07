dc3f37a163127cae8f1528c63267535558c73035
Andrea Pirazzini <info@help3d.it>
2021-01-07 16:31:07 +0100

    Aggiornamento V6

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(H3D - GENIUS MESH BED V5 - Piraz)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(H3D - GENIUS MESH BED V6 - Piraz)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -499,16 +499,10 @@
     #define DEFAULT_Ki_LIST {   0.89,   0.89 }
     #define DEFAULT_Kd_LIST { 41.93, 41.93 }
   #else
-    #define DEFAULT_Kp  12.19
-    #define DEFAULT_Ki   0.89
-    #define DEFAULT_Kd 41.93
+    #define DEFAULT_Kp  14.86
+    #define DEFAULT_Ki   1.08
+    #define DEFAULT_Kd 50.83
   #endif
-
-  // Sidewinder X1 / Genius
-  #define DEFAULT_Kp 12.19
-  #define DEFAULT_Ki 0.89
-  #define DEFAULT_Kd 41.93
-
 
 #endif // PIDTEMP
 
@@ -546,9 +540,9 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   //Bed originale Artillery
-  #define DEFAULT_bedKp 26.37
-  #define DEFAULT_bedKi 1.77
-  #define DEFAULT_bedKd 262.06
+  #define DEFAULT_bedKp 30.66
+  #define DEFAULT_bedKi 6.03
+  #define DEFAULT_bedKd 103.84
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
@@ -1400,7 +1394,7 @@
 
 // Homing speeds (mm/min)
 #define HOMING_FEEDRATE_XY (50*60)
-#define HOMING_FEEDRATE_Z  (15*60)
+#define HOMING_FEEDRATE_Z  (5*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
