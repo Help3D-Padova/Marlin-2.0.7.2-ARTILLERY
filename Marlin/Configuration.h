8d069d747772c151fafdb0750173d8e3f55a118b
Andrea Pirazzini <info@help3d.it>
2021-01-07 16:11:36 +0100

    Aggiornamento V6

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,8 +70,8 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(H3D - X1 MESH BED V5 - Piraz)" // Who made the changes.
-//#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
+#define STRING_CONFIG_H_AUTHOR "(H3D - X1 MESH BED V6 - Piraz)" // Who made the changes.
+#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
  * *** VENDORS PLEASE READ ***
@@ -499,15 +499,10 @@
     #define DEFAULT_Ki_LIST {   0.89,   0.89 }
     #define DEFAULT_Kd_LIST { 41.93, 41.93 }
   #else
-    #define DEFAULT_Kp  12.19
-    #define DEFAULT_Ki   0.89
-    #define DEFAULT_Kd 41.93
+    #define DEFAULT_Kp  16.58
+    #define DEFAULT_Ki   1.59
+    #define DEFAULT_Kd 43.12
   #endif
-
-  // Sidewinder X1 / Genius
-  #define DEFAULT_Kp 12.19
-  #define DEFAULT_Ki 0.89
-  #define DEFAULT_Kd 41.93
 
 
 #endif // PIDTEMP
@@ -1400,7 +1395,7 @@
 
 // Homing speeds (mm/min)
 #define HOMING_FEEDRATE_XY (50*60)
-#define HOMING_FEEDRATE_Z  (15*60)
+#define HOMING_FEEDRATE_Z  (5*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
