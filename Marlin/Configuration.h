085a6719e2666b4591522acff9758afc3702fcda
Andrea Pirazzini <info@help3d.it>
2021-01-09 11:44:40 +0100

    no motion

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1104,7 +1104,7 @@
 
 // @section homing
 
-#define NO_MOTION_BEFORE_HOMING // Inhibit movement until all axes have been homed
+//#define NO_MOTION_BEFORE_HOMING // Inhibit movement until all axes have been homed
 
 //#define UNKNOWN_Z_NO_RAISE      // Don't raise Z (lower the bed) if Z is "unknown." For beds that fall when Z is powered off.
 
