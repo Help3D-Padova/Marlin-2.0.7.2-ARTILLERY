9537d863b6ec8073f75554fb14106fa59dd3dd23
Andrea Pirazzini <info@help3d.it>
2021-01-09 23:31:59 +0100

    6.1

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "Marlin-2.0.7.2-V6-X1BL"
+#define SHORT_BUILD_VERSION "Marlin-2.0.7.2-V6.1-X1BL"
 
 /**
  * Verbose version identifier which should contain a reference to the location
