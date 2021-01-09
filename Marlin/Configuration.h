7cef222b93e4f5fd565929106d7e59f67c282738
Andrea Pirazzini <info@help3d.it>
2021-01-09 23:32:40 +0100

    6.1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(H3D - GENIUS BLTOUCH V6 - Piraz)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(H3D - GENIUS BLTOUCH V6.1 - Piraz)" // Who made the changes.
 #define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
